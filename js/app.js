122411d9662aa042113f8ab66e5c5f850f5f0298
mehedy3107 <mhador111@gmail.com>
2022-09-04 10:47:09 +0600

    Top Catagory Length Show Section Updated

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-// const url = `https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a`;
-// console.log(url);
 const loadMenu = async () => {
   const url = "https://openapi.programming-hero.com/api/news/categories";
   const res = await fetch(url);
@@ -7,106 +5,34 @@ const loadMenu = async () => {
   menu(data.data.news_category);
 };
 
-// const menu = () => {
-//   loader(true);
-//   const menu = document.getElementById("menu");
-
-//   const uniqueArray = [];
-//   for (const product of data) {
-//     if (uniqueArray.indexOf(product.category_name) === -1) {
-//       uniqueArray.push(product.strCategory);
-//       const li = document.createElement("li");
-//       li.innerHTML = `
-//               <a onclick="menuProductShow('${product.category_id}')">${product.category_name}</a>
-//               `;
-//       menu.appendChild(li);
-//     }
-//   }
-// };
-
 const menu = (news) => {
-  // console.log(news);
   const menu = document.getElementById("menu");
 
   news.forEach((product) => {
     const li = document.createElement("li");
 
     li.innerHTML = `
-      <a onclick="menuProductShow('${product.category_id}')">${product.category_name}</a>
+      <a onclick="menuProductShow('${product.category_id}', '${product.category_name}') ">${product.category_name}</a>
       `;
     menu.appendChild(li);
   });
 };
 
-// const loadApiProduct = async () => {
-//   const url = "https://openapi.programming-hero.com/api/news/category/08";
-//   const res = await fetch(url);
-//   const data = await res.json();
-//   return data.data;
-// };
-
-// const findProduct = async () => {
-//   const products = await loadApiProduct();
-//   const divId = document.getElementById("news-info");
-
-//   for (const news of products) {
-//     const createDiv = document.createElement("div");
-
-//     createDiv.innerHTML = `
-//     <div class="card card-side bg-pink-900 shadow-xl my-4">
-//     <img class="w-1/2" src="${news.image_url}" alt="Movie">
-//     <div class="card-body">
-//       <h2 class="card-title">${news.title}</h2>
-//       <p>${
-//         news.details.length > 220
-//           ? news.details.slice(0, 220) + "......."
-//           : news.title
-//       }</p>
-//       <div class="grid grid-cols-3 gap-4 ">
-//       <div class="flex">
-//       <img class="w-1/3 rounded-full mr-2" src="${news.author.img}">
-//       <div>
-//       <p>${news.author.name}</p>
-//       <p class="">${news.author.published_date}</p>
-//       </div>
-//       <div>
-//       </div>
-//       </div>
-//       <div class="flex justify-center items-center">
-//       <p class="flex justify-center items-center"><i class="fa-solid fa-eye mx-2"></i>  ${
-//         news.total_view
-//       }M</p>
-//       </div>
-
-//       <div class="flex justify-center items-center">
-//       <label for="my-modal-3" class="btn modal-button border-none bg-base-100"> <i class="fa-solid fa-circle-right"></i> </label>
-//       </div>
-//     </div>
-//     </div>
-//   `;
-//     divId.appendChild(createDiv);
-//   }
-// };
-
-const menuProductShow = async (id) => {
-  // const id0 = `0${id}`;
+const menuProductShow = async (id, name) => {
   const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
   const res = await fetch(url);
   const data = await res.json();
-  showProduct(data.data);
+  showProduct(data.data, name);
 };
 
-const showProduct = (data) => {
-  // console.log(data);
-
-  // const newsLength = data.length;
-  // const categoryFound = document.getElementById("category-found");
-  // categoryFound.innerHTML = newsLength;
-
+const showProduct = (data, name) => {
   const newsContainer = document.getElementById("news-container");
   newsContainer.innerHTML = "";
 
-  //   data.sort((s1. s2) => s2.total_view - s1.total_view).forEach(data => {
+  const categoryLength = document.getElementById("categoryLength");
+  categoryLength.innerText = data.length;
+  const catId = document.getElementById("catId");
+  catId.innerText = name;
 
   const divId = document.getElementById("news-info");
   divId.innerHTML = "";
@@ -163,11 +89,8 @@ const showProduct = (data) => {
       </div>
     `;
     newsId.appendChild(createDiv);
-    // console.log(product._id);
   }
-  // loader(false);
 };
-// };
 
 const modalProduct = async (news_id) => {
   const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
@@ -179,7 +102,6 @@ const modalProduct = async (news_id) => {
 };
 
 const modalProductShow = (modal) => {
-  // console.log(modal);
   const modalId = document.getElementById("modal-title");
   modalId.innerText = modal.title ? modal.title : "No Title Found";
 
@@ -200,27 +122,6 @@ const modalProductShow = (modal) => {
 //   } else {
 //     loadingBtn.classList.add("hidden");
 //   }
-// };
 
-// const showProductAmount = async (data) => {
-//   const categoryFound = document.getElementById("category-found");
-//   const menuLoad = await loadMenu();
-//   if (.length !== 0) {
-//     categoryFound.innerText = `Hi`;
-//   }
-// };
-// const modalAll = document.getElementById('modal');
-// modalAll.textContent = '';
-
-// modalAll.innerHTML = `
-
-// `
 loadMenu();
-menuProductShow("08");
-// menuProductShow();
-// menu();
-// loadApiProduct();
-// findProduct();
-// showProduct();
-// modalProduct();
-// modalProductShow();
+menuProductShow("08", "All News");
